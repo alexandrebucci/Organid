5613305a0f6d686a935be6b494beb9d634419d98
kilyan-s <sockalingum.kilyan@gmail.com>
2013-10-29 18:04:31 +0100

    Classes Tache et TacheManager
    Tache: creer un objet tache
    TacheManager: lien d'un tache avec la BDD
    testTache: teste d'un ajout de tache a la BDD
    Renomage de test.php en testUtilisateur.php

---
+++ utils/classes/TacheManager.php
@@ -0,0 +1,67 @@
+<?php
+class TacheManager
+{
+  private $db; // Instance de PDO.
+ 
+  public function __construct($db)
+  {
+    $this->setDb($db);
+  }
+  //Ajour d'une tache à la base de donnee
+  public function add(Tache $tache)
+  {
+    $q = $this->db->prepare('INSERT INTO taches (`Nom`, `Difficulte`, `Description`, `Id_Cat`, `Id_U`) VALUES (:nom, :difficulte, 
+    :description, :id_Cat, :id_U)');
+ 
+    $q->execute(array(
+      ":nom" => $tache->nom(),
+      ":difficulte" => $tache->difficulte(),
+      ":description" => $tache->description(),
+      ":id_Cat" => $tache->id_Cat(),
+      ":id_U" => $tache->id_U()
+    ));
+  }
+ 
+  public function delete(Tache $tache)
+  {
+    // Exécute une requête de type DELETE.
+  }
+ 
+  public function get($id)
+  {
+    // Exécute une requête de type SELECT avec une clause WHERE, et retourne un objet Tache.
+    $id = (int) $id;
+ 
+    $q = $this->db->query('SELECT * FROM taches WHERE Id_Tm = '.$id);
+    $donnees = $q->fetch(PDO::FETCH_ASSOC);
+ 
+    return new Tache ($donnees);
+  }
+ 
+  public function getList()
+  {
+    // Retourne la liste de tous les personnages.
+  }
+ 
+  public function update(Tache $tache)
+  {
+    //preparation de la requete avec PDO
+    $q = $this->db->prepare('UPDATE taches SET (`Nom`, `Difficulte`, `Description`, `Id_Cat`, `Id_U`) VALUES (:nom, :difficulte, 
+    :description, :id_Cat, :id_U) WHERE `Id_Tm` = :id');
+    //Asignation des valeurs
+    $q->execute(array(
+      ":nom" => $tache->nom(),
+      ":difficulte" => $tache->difficulte(),
+      ":description" => $tache->description(),
+      ":id_Cat" => $tache->id_Cat(),
+      ":id_U" => $tache->id_U(),
+      ":id" =>$tache->id()
+    ));
+  }
+ 
+  public function setDb(PDO $db)
+  {
+    $this->db = $db;
+  }
+}
+?>
